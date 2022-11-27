86af9f0602905f6d3df03c35b45c1364e7d75afc
woaidouya123 <cfeigood@qq.com>
2022-11-27 22:51:11 +0800

    feat: 背景组件化

---
+++ src/components/background.py
@@ -0,0 +1,9 @@
+import os
+from PIL import Image
+from kindleWidget import KindleWidget
+
+imgPath = os.path.abspath(os.path.join(os.path.dirname(__file__), "../assets/background/"))
+
+class Background(KindleWidget):
+  def __init__(self, s_width, s_height, s_rotate, left=0, top=0, isKindle=False) -> None:
+    super().__init__(s_width, s_height, s_rotate, left, top, isKindle)
